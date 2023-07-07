2122c025ba30a3b7289aea7a3fa9d0b87d6becb4
aarth19 <aarthpatel19@gmail.com>
2023-07-07 13:37:35 -0400

    Adding legacy waifu function

---
+++ commands/fun/waifu.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("waifu")
+    .setDescription("Replies with Waifu!"),
+  async execute(interaction) {
+    fetch('https://api.waifu.pics/sfw/waifu')
+        .then(response => {
+            return response.json();
+        })
+        .then(async url => {
+            await interaction.reply(url["url"]);
+        })
+  },
+};
