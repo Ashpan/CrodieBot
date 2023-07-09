c59762bec20dae93a989ebd214f757d4077c6729
Ashpan Raskar <Ashpanr@gmail.com>
2023-07-09 15:03:26 -0400

    Fixed regression that errored with import

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
 const { token } = require("./config.json");
-const { handleReactionAdd, handleReactionRemove } = require('./events/messageReactionHandler');
 
 // Create a new client instance
 const client = new Client({
